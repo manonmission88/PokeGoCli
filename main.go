49c616d69f59a5498f998e8304dd711322eb0ee1
manonmission88 <manishnewray88@gmail.com>
2025-07-07 23:29:10 -0400

    added help and exit command

--- main.go
+++ main.go
@@ -20,19 +20,69 @@ func cleanInput(text string) []string {
 	return finalResult
 
 }
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+// mapping commands and features
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+// method to exit the repl
+func commandExit() error {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+// method for help
+func commandHelp() error {
+	fmt.Println("Available commands:")
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
 func main() {
 	// read the cli input
 	reader := bufio.NewScanner(os.Stdin)
+	fmt.Printf("Welcome to the Pokedex!\n")
 	for {
-		fmt.Printf("Pokedex > ")
 		reader.Scan()
 		inputText := reader.Text()
 		words := cleanInput(inputText)
-		if len(words) > 0 {
-			fmt.Printf("Your command was: %s\n", words[0])
-		} else {
-			fmt.Println("Please type a command.")
+		if len(words) == 0 {
+			continue
 		}
+		command := words[0]
+		cmd, ok := getCommands()[command]
+		if ok {
+			err := cmd.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+
 	}
 
 }
