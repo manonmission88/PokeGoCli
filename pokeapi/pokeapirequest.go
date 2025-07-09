4b41bef7b2a881f638ae620525b43848d8870416
manonmission88 <manishnewray88@gmail.com>
2025-07-09 00:17:45 -0400

    added function to make api call

---
+++ pokeapi/pokeapirequest.go
@@ -0,0 +1,40 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const (
+	baseURL = "https://pokeapi.co/api/v2"
+)
+
+// locations list
+func (c *Client) CallLocation(pageUrl *string) (InnerLocations, error) {
+	url := baseURL + "/location-area"
+	if pageUrl != nil {
+		url = *pageUrl
+	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return InnerLocations{}, err
+	}
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return InnerLocations{}, err
+	}
+	// unmarshall
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return InnerLocations{}, err
+	}
+	locationResp := InnerLocations{}
+	err = json.Unmarshal(data, &locationResp)
+	if err != nil {
+		return InnerLocations{}, err
+	}
+	return locationResp, nil
+
+}
